f7d387c0ab4dfaf7ec609fddbc6904ed6268d1d4
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-08 11:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let hm={};
+	for(let char of str){
+		hm.set(char,(hm.get(char) || 0)+1);
+	}
+	for(let [key,value] of hm.entries()){
+		if(value === 1){
+			return `${key}`;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
